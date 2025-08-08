c7d1a5f4e3e8ee1e958745d923fcd067927e9ef8
Neha Gupta <neha.gupta@thoughtworks.com>
2025-08-08 10:21:57 +0530

    Updating the customer class with required constructor, getter and setters.

--- src/main/java/com/tw/joi/delivery/model/Customer.java
+++ src/main/java/com/tw/joi/delivery/model/Customer.java
@@ -6,4 +6,41 @@ public class Customer {
     private String lastName;
     private String loyaltyPoints;
     private CustomerSegment loyaltySegment;
+
+    public Customer() {
+    }
+
+    public Customer(String customerId, String firstName, String lastName) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setLoyaltyPoints(String loyaltyPoints) {
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    public void setLoyaltySegment(CustomerSegment loyaltySegment) {
+        this.loyaltySegment = loyaltySegment;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    public CustomerSegment getLoyaltySegment() {
+        return loyaltySegment;
+    }
 }
